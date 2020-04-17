33dea31cb16898033398a10d4472bb7af9574d3f
Virginia Davenport <virginiadavenport@Virginias-MBP.hsd1.nm.comcast.net>
2020-04-16 22:50:05 -0600

    15 passing

--- index.js
+++ index.js
@@ -273,8 +273,14 @@ function getLastCarInfo(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+  let yearArray = [];
+  for(let i = 0; i < inventory.length; i++){
+  //if(inventory[i]=== car_year){
+    yearArray.push(inventory[i].car_year);
+  }
+  return yearArray;
+  
 }
 
 /**
